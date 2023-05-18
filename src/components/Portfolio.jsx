9ef125cf873e3d9c8ae44fec3e15fd05b91dbf7b
Eswhar <chalamallan3@gmail.com>
2023-05-18 11:56:41 +0530

    project added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,6 +7,7 @@ import MatchGame from '../assets/portfolio/MatchGame.png'
 import digitaltimer from '../assets/portfolio/digitaltimer.png'
 import passwordmanager from '../assets/portfolio/passwordmanager.png'
 import todo from '../assets/portfolio/todo.png'
+import tictactoe from '../assets/portfolio/tictactoe.png'
 
 const Portfolio=()=>{
     const portfolio=[
@@ -14,51 +15,75 @@ const Portfolio=()=>{
             id:1,
             src:foodmuch,
             demo:'https://eshwarfoodpage.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/Food-Munch'
+            code:'https://github.com/ESHWAR-333/Food-Munch',
+            title:'FoodMunch',
+
         },
         {
             id:2,
-            src:EmojiGame,
-            demo:'https://mojiesgame.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/Emoji-Game-React-js'
+            src:moneyManagerApp, 
+            demo:'https://moneymanganer.ccbp.tech/',
+            code:'https://github.com/ESHWAR-333/-Money-Manager-App-React-JS',
+            title:'Money Manager App',
 
 
         },
         {
             id:3,
-            src:moneyManagerApp, 
-            demo:'https://moneymanganer.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/-Money-Manager-App-React-JS'
+            src:passwordmanager,
+            demo:'https://pwdsmanager.ccbp.tech/',
+            code:'https://github.com/ESHWAR-333/Password-Manager-React-js',
+            title:'Password Manager',
+
 
         },
         {
             id:4,
-            src:MatchGame,
-            demo:'https://eshwarmatchgm.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/Match-Game-React-js' 
+            src:EmojiGame,
+            demo:'https://mojiesgame.ccbp.tech/',
+            code:'https://github.com/ESHWAR-333/Emoji-Game-React-js',
+            title:'Emoji Game',
+
+
 
         },
         {
             id:5,
-            src:digitaltimer,
-            demo:'https://countdigi.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/Digital-Timer-App-React-JS' 
+            src:tictactoe,
+            demo:'https://eshwar-333.github.io/tic-tac-toe/',
+            code:'https://github.com/ESHWAR-333/tic-tac-toe' ,
+            title:'Tic Tac Toe',
 
         },
         {
             id:6,
-            src:passwordmanager,
-            demo:'https://pwdsmanager.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/Password-Manager-React-js' 
+            src:MatchGame,
+            demo:'https://eshwarmatchgm.ccbp.tech/',
+            code:'https://github.com/ESHWAR-333/Match-Game-React-js' ,
+            title:'Match Game',
+
 
         },
         {
             id:7,
+            src:digitaltimer,
+            demo:'https://countdigi.ccbp.tech/',
+            code:'https://github.com/ESHWAR-333/Digital-Timer-App-React-JS' ,
+            title:'Digital Timer',
+
+
+        },
+        
+        {
+            id:8,
             src:todo,
             demo:'https://eshwartodos.ccbp.tech/',
-            code:'https://github.com/ESHWAR-333/todo-application-javascript' 
+            code:'https://github.com/ESHWAR-333/todo-application-javascript' ,
+            title:'Todo Application',
 
-        }
+
+        },
+        
     ]
     
 
@@ -72,7 +97,9 @@ const Portfolio=()=>{
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
                     {portfolio.map(each=>(<div key={each} className="shadow-md shadow-gray-600 rounded-lg ">
                         <img src={each.src} alt="" className="rounded-md duration-200 hover:scale-105"/>
+                        <p className="text-center text-gray-500 font-medium pt-2">{each.title}</p>
                         <div className="flex items-center justify-center">
+                        
                             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"><a href={each.demo}>Demo</a></button>
                             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"><a href={each.code}>Code</a></button>
                         </div>
